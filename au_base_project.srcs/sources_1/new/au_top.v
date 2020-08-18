b0abb1dae99986657987ed2424e3e066ce24a14c
edi <edi.gotlieb@gmail.com>
2020-08-19 00:45:44 +0300

    added i2c parser...does something? #todo - add watchdog on FSM

--- au_base_project.srcs/sources_1/new/au_top.v
+++ au_base_project.srcs/sources_1/new/au_top.v
@@ -3,46 +3,34 @@ module au_top(
     input rst_n,
     output[7:0] led,
     input usb_rx,
-    output usb_tx
+    output usb_tx,
+    
+    input i2c_scl,
+    input i2c_sda,
+    
+    output debug0,
+    output debug1,
+    output debug2,
+    output debug3
     );
     
-    wire rst;          
+    reg reset;          
+    wire rst;
     
     reset_conditioner reset_conditioner(.clk(clk), .in(!rst_n), .out(rst));    
-       
+      
+    
+    
     assign usb_tx = usb_rx;
-   
-   wire [27:0] counter_value;
-   
-  counter #(.CTR_LEN(28)) counter (
-    .rst(rst),
-    .clk(clk),
-    .dir(1'b1),
-    .reverse_on_carry(1'b1),
-    .start_value(28'b0),
-    .counter_value(counter_value)
-    );
 
-  /*  
-  pwm #(.CTR_LEN(8)) pwm_generator (
-    .rst(rst),
-    .clk(clk),
-    .compare(counter_value[29:22]),
-    .pwm(led[0])
-  );
-   */
-   genvar i;
-   generate
-        for (i = 0; i < 8; i=i+1) begin: pwm_gen_loop
-        pwm #(.CTR_LEN(8)) pwm (
-          .rst(rst),
-          .clk(clk),
-          .compare(counter_value[27:20]),
-          .pwm(led[i])
-        );
-        end
-   endgenerate
+    always @(posedge clk)
+        reset <= rst;
     
+    i2c_state_parser i2c_state_parser(reset,clk,i2c_scl,i2c_sda,debug0,debug1,debug2,debug3);
     
+    assign led[0] = debug0;
+    assign led[1] = debug1;
+    assign led[2] = debug2;
+    assign led[3] = debug3;
     
 endmodule
--- au_base_project.srcs/sources_1/new/reset_conditioner.v
+++ au_base_project.srcs/sources_1/new/reset_conditioner.v
@@ -8,6 +8,21 @@ module reset_conditioner (
     output reg out
   );
   
+  
+  reg[3:0] storage = 4'hf;
+  reg[1:0] in_synced = 2'b00; 
+  
+  always @(posedge clk) begin
+    if(in_synced[1] == 1'b1) begin
+        storage <= 4'hf;
+        in_synced <= 2'b00;
+    end else
+        storage <= { storage[2:0], 1'b0 };
+    in_synced <= { in_synced[0], in };
+    out <= storage[3];    
+  end
+  
+  /*
   localparam STAGES = 3'h4;
   
   
@@ -27,5 +42,5 @@ module reset_conditioner (
       M_stage_q <= M_stage_d;
     end
   end
-  
+  */
 endmodule
